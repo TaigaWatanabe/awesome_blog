88cf0c580b7f8a74e9e6515c25ef418d0e5dd430
Taiga <1208taiga@gmail.com>
2021-10-05 20:31:04 +0900

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create,:destroy]
   resources :relationships, only: [:create, :destroy]
